00bb7623e36c2b32fcbc464a9e35d72d04dccf3e
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2022-07-12 16:27:39 -0500

    nuevos cambios

--- src/main/java/pe/edu/idat/controller/LoginController.java
+++ src/main/java/pe/edu/idat/controller/LoginController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,24 +24,28 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
-    public ModelAndView login(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
+    //@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    @GetMapping(value={"/", "/login"})
+    public String login(){
+        //ModelAndView modelAndView = new ModelAndView();
+        //modelAndView.setViewName("login");
+        return "login";
     }
 
 
-    @RequestMapping(value="/registration", method = RequestMethod.GET)
-    public ModelAndView registration(){
-        ModelAndView modelAndView = new ModelAndView();
+    //@RequestMapping(value="/registration", method = RequestMethod.GET)
+    @GetMapping("/registration")
+    public String registration(Model model){
+        //ModelAndView modelAndView = new ModelAndView();
         User user = new User();
-        modelAndView.addObject("user", user);
-        modelAndView.setViewName("registration");
-        return modelAndView;
+        //modelAndView.addObject("user", user);
+        //modelAndView.setViewName("registration");
+        model.addAttribute("user", user);
+        return "registration";
     }
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    //@RequestMapping(value = "/registration", method = RequestMethod.POST)
+    @PostMapping("/registration")
     public ModelAndView createNewUser(User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         User userExists = userService.findUserByUserName(user.getUsername());
@@ -53,20 +61,24 @@ public class LoginController {
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
             modelAndView.setViewName("registration");
-
+          
         }
         return modelAndView;
     }
 
-    @RequestMapping(value="/admin/home", method = RequestMethod.GET)
-    public ModelAndView home(){
-    	ModelAndView modelAndView = new ModelAndView();
+    //@RequestMapping(value="/admin/home", method = RequestMethod.GET)
+    @GetMapping("/admin/home")
+    //public ModelAndView home(){
+    public String home(Model model){
+    	//ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByUserName(auth.getName());
-        modelAndView.addObject("username", "Welcome " + user.getUsername() + "/" + user.getName() + " " + user.getLastname() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-        modelAndView.setViewName("admin/home");
-        return modelAndView;
+        model.addAttribute("userName", "Welcome " + user.getUsername() + "/" + user.getName() + " " + user.getLastname() + " (" + user.getEmail() + ")");
+        model.addAttribute("adminMessage","Content Available Only for Users with Admin Role");
+        //modelAndView.addObject("username", "Welcome " + user.getUsername() + "/" + user.getName() + " " + user.getLastname() + " (" + user.getEmail() + ")");
+        //modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        //modelAndView.setViewName("admin/home");
+        return "admin/home";
     }
 	
 }
